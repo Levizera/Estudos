e8eb60dc185a7e1f59fcd305f978a56fe488e840
Levizera <levigames321@gmail.com>
2021-03-14 17:17:12 -0300

    Adding study folder in this repo

---
+++ Python/Learning/lesson_1.py
@@ -0,0 +1,52 @@
+# Multiplicação
+tax = 12.5 / 100
+price = 100.50
+mult = tax * price
+
+
+# Arredondando numeros
+r = round(mult)
+
+
+# print(f'O resultado da multiplicação de {tax} + {price} é igual a {mult}')
+# print(f'O arredondamento de {mult} é {r}')
+
+
+# Adicionando "", '' em strings
+frase = 'Don\'t be a Fool'
+segunda_frase = 'I \"can\" do it'
+breaking_lines = 'First Line\nSecond line'
+
+# Printing a string that\has\multiple\lines
+dont_break_lines = r'cd /sdcard/examples'
+
+
+# Kinda multiplying strings
+multi_string = 'ba' + 2 * 'na'
+
+
+# Concatenating and adding strings
+prefix = 'Le'
+whole_name = prefix + 'vi'
+
+
+# Looking a value in a string or Array
+array = ['Levi', 'Pedro', 'Dionisio']
+'''print(array[0])'''
+
+string = 'Levi'
+'''print(string[0])'''
+# Looking at the last character
+'''print(string[-1])'''
+
+
+# Getting the length of a string
+s = 'asdfasdfasdfasdfa'
+'''print(len(s))'''
+
+
+
+
+
+
+
