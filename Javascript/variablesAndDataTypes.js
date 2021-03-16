6837fd441f34a26dc99d0452bed17eabe5e967ee
Levizera <levigames321@gmail.com>
2021-03-16 09:46:13 -0300

    adicionando pasta js

---
+++ Javascript/variablesAndDataTypes.js
@@ -0,0 +1,13 @@
+// String variables
+var name = 'Levi';
+var phrase = 'Hello world';
+
+// Number variables
+let age = 17;
+let pi = 3.14;
+
+//Boolean variables
+let isMale = true;
+let isWoman = false;
+let nothing = null;
+let description = undefined;
---
+++ Javascript/workingWithStrings.js
@@ -0,0 +1,41 @@
+//String length: Para ver a quantidade de caracteres dentro de uma variavel, basta usar 
+// --> console.log(myName.length);
+let myName = 'Levi Ferreira da Silva';
+
+
+//Formantando uma string para UPPERCASE
+let UPPERCASE = 'hey there, my name is levi';
+let lowercase = 'HEY THERE HOW YOU HAVE BEEN DOING?';
+//Formantando para UPPPERCASE --> console.log(hello.toUpperCase());
+//Formantando para lowercase --> console.log(lowercase.toLowerCase());
+
+
+// Localizando caracteres dentro de uma string: 
+// --> console.log(localizandoCaracter.charAt(0));
+let localizandoCaracter = 'Localiza ai BB';
+
+// Descobrindo qual a posição de um caracter dentro de uma string
+// --> console.log(localizandoCaracter.indexOf('L'));
+
+//Pegando mais de um index de dentro de uma string
+let yourName = 'levizera oficial';
+
+// Esse metodo basicamente pega partes especificas de uma string 
+// --> console.log(yourName.substring(0, 8));
+
+// Como saber se uma string termina com certo valor
+let historyOfMe = 'Thats a history of my life';
+
+// --> console.log(historyOfMe.endsWith('life'));
+// Tambem pode ser usado o metodo: 
+// --> console.log(historyOfMe.includes('life'));
+
+
+// Juntando duas variaveis/strings
+let prefix = 'Eu me chamo ';
+let sufix = 'Levi';
+// --> console.log(prefix + sufix);
+
+
+// To know more about Strings Methods in javascript
+// Just search: W3school Strings Methods Javascript 
