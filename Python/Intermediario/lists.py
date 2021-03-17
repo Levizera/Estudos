47ee547275c7126a4e9cba4cf1abf82f697f9941
Levizera <levigames321@gmail.com>
2021-03-17 15:59:19 -0300

    pasta de python com codes level intermediarios

---
+++ Python/Intermediario/lists.py
@@ -0,0 +1,4 @@
+listas_2d = [['levi', 'ferreira', 'da', 'silva'], [1, 2, 3, 4, 5], ['pedro', 'lucas', 'ferreira'  ] ]
+
+for i in listas_2d:
+    print(i)
---
+++ Python/Intermediario/sets.py
@@ -0,0 +1,58 @@
+'''
+--> O que é um set?? <--
+--> Os sets são uma coleção de itens desordenada, imutável e que não podem conter elementos duplicados. Por ser parcialmente imutável, os sets possuem permissão de adição e remoção de elementos.
+'''
+
+
+# --> Set básico
+set_classico = {'levi', 1, True, 32, 'ferreira', 7,  False, 23}
+
+
+# --> Set que divide as letras de uma string
+dividindo_letras = set('Levi')
+
+
+# --> Set vazio
+set_vazio = set()
+
+
+# --> Adicionando valores ao set
+# --> Somente um valor pode ser adicionado por vez nome_do_set.add(valor)
+adicionando_valores = set()
+
+# --> adicionando_valores.add(1)
+# --> adicionando_valores.add('levi')
+# --> adicionando_valores.add('ferreira')
+
+
+# --> Removendo valores
+# --> removendo_valores.remove('levi')
+removendo_valores = {'levi', 1, 'ferreira', 2}
+
+
+# --> Discartando elemtentos de dentro do set
+# --> discartando_elementos.discard('levi')
+discartando_elementos = {'levi', 'ferreira', 'da', 'silva'}
+
+
+# --> Limpando um set 
+# --> limpando_elementos.clear()
+limpando_elementos = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+
+# --> Tirando elemento do set e mostrando ele 
+# --> mostrando_elemento.pop()
+mostrando_elemento = {'levi', 'ferreira', 'silva'}
+
+
+# --> Juntando 2 sets
+# --> unindo = impar.union(par)
+
+impar = {1, 3, 5, 7, 9}
+par = {2, 4, 6, 8}
+unindo = impar.union(par)
+
+
+# Vendo os valores iguais de dois sets
+
+
